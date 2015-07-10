653b46e0a53ec6f9cbb98d26d2088d56b34c2f82
Ferris Tseng <ferristseng@gmail.com>
2015-07-10 15:15:35 -0400

    commit docs for 1.0.0

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['freqdist'] = ["impl&lt;K, H = <a class='struct' href='http://doc.rust-lang.org/nightly/core/hash/sip/struct.SipHasher.html' title='core::hash::sip::SipHasher'>SipHasher</a>, S = <a class='struct' href='http://doc.rust-lang.org/nightly/std/collections/hash/state/struct.DefaultState.html' title='std::collections::hash::state::DefaultState'>DefaultState</a>&lt;<a class='struct' href='http://doc.rust-lang.org/nightly/core/hash/sip/struct.SipHasher.html' title='core::hash::sip::SipHasher'>SipHasher</a>&gt;&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='freqdist/struct.FrequencyDistribution.html' title='freqdist::FrequencyDistribution'>FrequencyDistribution</a>&lt;K, S&gt;",];implementors['rand'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='rand/reseeding/struct.ReseedWithDefault.html' title='rand::reseeding::ReseedWithDefault'>ReseedWithDefault</a>",];implementors['num'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='num/bigint/struct.BigUint.html' title='num::bigint::BigUint'>BigUint</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='num/bigint/struct.BigInt.html' title='num::bigint::BigInt'>BigInt</a>",];implementors['punkt'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='punkt/struct.TrainingData.html' title='punkt::TrainingData'>TrainingData</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ punkt/tokenizer/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["SentenceByteOffsetTokenizer","Iterator over the byte offsets of a document."],["SentenceTokenizer","Iterator over the sentence slices of a document."]]});
